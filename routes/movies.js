c6a888d3da3028671ffa977b8be6a7143a08620d
Alexsandr Chigarev <majestik13@mail.ru>
2021-03-27 00:25:28 +0300

    reset

--- routes/movies.js
+++ routes/movies.js
@@ -7,5 +7,21 @@ const {
 router.get('/movies', getMovie);
 router.post('/movies', validateMovie, createMovie);
 router.delete('/movies/:movieId', validateDeleteMovie, deleteMovie);
+// router.put('/movies/:movieId/likes', likeMovies);
+// router.delete('/movies/:movieId/likes', dislikeMovies);
+
+// router.put('/movies/:movieId/likes', celebrate({
+//   params: Joi.object().keys({
+//     cardId: Joi.string().alphanum().required().length(24)
+//       .hex(),
+//   }),
+// }), likeMovies);
+//
+// router.delete('/movies/:movieId/likes', celebrate({
+//   params: Joi.object().keys({
+//     cardId: Joi.string().alphanum().required().length(24)
+//       .hex(),
+//   }),
+// }), dislikeMovies);
 
 module.exports = router;
